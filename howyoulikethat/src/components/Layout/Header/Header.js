06ba06f628327b6addc28b42a9713a58b4694f52
BOGUENSONG <idsonic95@gmail.com>
2023-02-01 22:39:04 +0900

    add bootstrap and change menu

--- howyoulikethat/src/components/Layout/Header/Header.js
+++ howyoulikethat/src/components/Layout/Header/Header.js
@@ -4,17 +4,42 @@ import styles from "../../../design/Header.css"
 const Header = () => {
     return (
         <header className={styles.header}>
-            <div className={styles.contents}>
-                <h1 className={styles.logo}>
-                    <img width="200px" src={logo} className="App-logo" alt="logo" />
-                </h1>
-                <nav className={styles.navigation}> 
-                    <ul>
-                        <li>Store</li>
-                        <li>Shop</li>
-                    </ul>
-                </nav>
-            </div>
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="!#"><img width="200px" src={logo} className="App-logo" alt="logo" /></a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="!#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="!#">Link</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false" href="!#">
+                Dropdown
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="!#">Action</a></li>
+                <li><a className="dropdown-item" href="!#">Another action</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="!#">Something else here</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="!#">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
         </header>
     )
 }
