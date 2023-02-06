000c1cd016e5f589bbffbaf17358cd7b5c654970
BOGUENSONG <idsonic95@gmail.com>
2023-02-06 21:05:37 +0900

    Change

--- howyoulikethat/src/containers/Home/HomeContainer.js
+++ howyoulikethat/src/containers/Home/HomeContainer.js
@@ -1,13 +1,29 @@
 import axios from "axios";
+import React, { useState, useEffect } from 'react';
+// const ApiTest = () => {
+//     return axios.get('localhost:5000/Hello?name=LordYuk');
+// }
 
-const ApiTest = () => {
-    return axios.get('localhost:5000/Hello?name=LordYuk');
+async function ApiTest () {
+    return await axios.get('http://localhost:5000/Hello?name=LordYuk');
 }
-const HomeContainer = () => {
 
+
+const HomeContainer = () => {
+    const [ApiValue, setApiValue] = useState(null); 
+    
+    useEffect( () => {
+            const response = ApiTest().then( function () {
+            }).catch(function (error) {
+                console.log(error)
+            })  ;
+            setApiValue(response);
+
+    }, [])
+    
     return (
         <div>
-            {ApiTest()}
+            { ApiValue }
         </div>
 
     )
