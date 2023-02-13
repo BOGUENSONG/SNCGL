d364dca9f2d08a2912ec02ccc4b4f50afd0686cc
BOGUENSONG <idsonic95@gmail.com>
2023-02-13 21:13:47 +0900

    add swiper library

--- react/src/containers/Home/HomeContainer.js
+++ react/src/containers/Home/HomeContainer.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 import React, { useState, useEffect } from 'react';
-// const ApiTest = () => {
-//     return axios.get('localhost:5000/Hello?name=LordYuk');
-// }
+import Container from 'react-bootstrap/Container';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import SwiperCore, { Navigation, Pagination } from "swiper";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
 
+
+import pic1 from '../../design/images/image1.jpg';
+import pic2 from '../../design/images/image2.jpg';
+SwiperCore.use([Navigation, Pagination])
 async function ApiTest () {
     return await axios.get('backend/Hello?name=LordYuk');
 }
@@ -24,13 +31,21 @@ const HomeContainer = () => {
     }, [])
     
     return (
-        <div>
-            Api Result:  {ApiValue} 
-            <div>
-                <img src=""/>
-            </div>
+        <Container className="panel" >
+            <Swiper
+                className="banner"
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+            >
+                <SwiperSlide>Api Result: {ApiValue}<br/><br/><br/><br/><br/><br/><br/><br/></SwiperSlide>
+                <SwiperSlide><img src={pic1} alt="pic1 logo"/></SwiperSlide>
+                <SwiperSlide><img src={pic2} alt="pic2 logo"/></SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
             
-        </div>
+        </Container>
         
 
     )
