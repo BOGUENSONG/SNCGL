086c3269b22ce6c767cf7d61cd68173ca9cdfa21
BOGUENSONG <idsonic95@gmail.com>
2023-03-01 20:05:51 +0900

    register end

--- react/src/containers/Register.js
+++ react/src/containers/Register.js
@@ -1,18 +1,18 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import logo from "../design/images/Logo.png";
+import logo from "../design/images/Logo_nobg.png";
 const Register = () => {
   const style = {
     width: "500px",
     height: "auto",
   };
   const imgstyle = {
-    width: "350px",
+    width: "300px",
   };
   return (
     <div className="d-flex justify-content-center">
       <div className="position-absolute">
-        <div className="bg-black d-flex justify-content-center">
+        <div className="bg-black p-3 d-flex justify-content-center">
           <img src={logo} alt="logo" style={imgstyle} />
         </div>
         <div className="p-4 bg-black" style={style}>
@@ -34,6 +34,29 @@ const Register = () => {
                 <Form.Label htmlFor="phone">전화번호</Form.Label>
                 <Form.Control id="phone" placeholder="전화번호" />
               </Form.Group>
+              <div>회원약관</div>
+              <textarea
+                style={{ resize: "none" }}
+                readOnly
+                class="form-control"
+                id="exampleFormControlTextarea1"
+                rows="5"
+              >
+                {
+                  "나중에api로 가져올자리\n줄바꿈확인\n확인3\n확인4\n확인5\n확인6"
+                }
+              </textarea>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckChecked"
+                />
+                <label className="form-check-label" for="flexCheckChecked">
+                  약관에 동의합니다.
+                </label>
+              </div>
               <div className="row">
                 <Button className="w-40 m-3 col " type="submit">
                   회원가입
